392d08de7101a0c35caa6570f1bd2b0fe5ed1482
PedroHV72 <pedroh.cpv@hotmail.com>
2022-03-14 13:42:08 -0300

    Criando app, adicionando conteúdo e criando navegação

---
+++ src/pages/Home.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { View, StyleSheet, ScrollView, TextInput, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import New from '../components/New';
+
+export default function Home() {
+
+const navigation = useNavigation();
+
+ return (
+   <ScrollView showsVerticalScrollIndicator={false} style={{ backgroundColor:'#FFF' }}>
+       <View style={styles.header}>
+            <View style={styles.inputArea}>
+                <Feather 
+                    name='search' size={24} color='black'
+                />
+                <TextInput 
+                    placeholder='O que está procurando?'
+                    style={styles.input}
+                />
+            </View>
+       </View>
+
+       <View style={styles.contentNew}>
+           <Text style={styles.title}>Novidades</Text>
+       </View>
+
+       <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ paddingHorizontal: 15 }}>
+            <New 
+                cover={require('../assets/house1.jpg')}
+                name='Casa de Praia'
+                description='Casa nova uma quadra do mar, lugar seguro e monitorado 24horas.'
+                onPress={() => navigation.navigate('Detail') }
+            />
+
+            <New 
+                cover={require('../assets/house2.jpg')}
+                name='Casa Floripa'
+                description='Casa nova uma quadra do mar, lugar seguro e monitorado 24horas.'
+                onPress={() => navigation.navigate('Detail') }
+            />
+
+            <New 
+                cover={require('../assets/house3.jpg')}
+                name='Guarujá'
+                description='Casa nova uma quadra do mar, lugar seguro e monitorado 24horas.'
+                onPress={() => navigation.navigate('Detail') }
+            />
+       </ScrollView>
+   </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    header: {
+        paddingHorizontal: 15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        marginVertical: 30
+    },
+    inputArea: {
+        paddingHorizontal: 15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '98%',
+        backgroundColor: '#FFF',
+        elevation: 2,
+        paddingHorizontal: 10,
+        height: 37,
+        borderRadius: 10
+    },
+    input: {
+        fontFamily: 'Montserrat_500Medium',
+        paddingHorizontal: 10,
+        fontSize: 13,
+        width: '90%'
+    },
+    contentNew: {
+        flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center'
+    },
+    title: {
+        paddingHorizontal: 15,
+        fontFamily: 'Montserrat_700Bold',
+        fontSize: 18,
+        color: '#4f4a4a'
+    }
+})
